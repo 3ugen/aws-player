500c72bfbe2be9574779a8dcdf16ef6da84c470c
Eugen Pyatygin <eugen.pyatygin@gmail.com>
2021-10-27 19:07:52 +0500

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import AppBar from '@mui/material/AppBar'
+// import { styled } from '@mui/system'
+import React from 'react'
+import {Container, Toolbar} from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import Home from '@mui/icons-material/Home'
+import MuiNextLink from '@components/MuiNextLink'
+import {styled} from "@mui/material/styles";
+
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar)
+
+export const navLinks = [
+  {title: `home`, path: `/`},
+  {title: `about us`, path: `/about-us`},
+];
+
+const Header: React.FC = () =>
+  <>
+    <AppBar position='fixed'>
+      <Toolbar>
+        <Container
+          maxWidth='lg'
+          sx={{display: `flex`, justifyContent: `space-between`}}
+        >
+          <IconButton edge='start' aria-label='home'>
+            <MuiNextLink activeClassName='active' href='/'>
+              <Home
+                sx={{
+                  color: (theme) => theme.palette.common.white
+                }}
+                fontSize='large'
+              />
+            </MuiNextLink>
+          </IconButton>
+        </Container>
+      </Toolbar>
+    </AppBar>
+    <Offset />
+  </>
+
+export default Header
---
+++ src/pages/index.tsx
@@ -0,0 +1,80 @@
+import Container from '@mui/material/Container';
+
+import React, {useEffect, useRef, useState} from 'react'
+import ReactHowler from 'react-howler'
+import {Typography} from "@mui/material";
+import {Pause, PlayArrow} from "@mui/icons-material";
+import Button from "@mui/material/Button";
+
+const Index: React.FC = () => {
+
+  // const [isOn, toggleIsOn] = useToggle(true);
+  const [isPlaying, togglePlaying] = useState(false);
+
+  const musicFile = 'assets/music/sound.ogg'
+
+  const [duration, setDuration] = useState(0);
+  const [seek, setSeek] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+
+  let player = useRef(null);
+
+  useEffect(() => {
+    let interval = null;
+    if (isPlaying) {
+      interval = setInterval(() => {
+        setSeek(player.seek());
+      }, 100);
+    } else if (!isPlaying && seek !== 0) {
+      clearInterval(interval);
+      setSeek(0)
+    }
+    return () => clearInterval(interval);
+  }, [isPlaying, seek]);
+
+  const handleClick = () => {
+    togglePlaying(!isPlaying)
+  }
+
+  function handleOnEnd() {
+    console.log('onEnd')
+    if(isPlaying) {
+      tooglePlaying()
+    }
+  }
+  function handleOnLoad() {
+    console.log('onLoad')
+    if(duration === 0) {
+      setDuration(player.duration())
+    }
+    if (!isLoaded) {
+      setLoaded(true)
+    }
+  }
+
+  function handlePause() {
+    console.log('onPause')
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="body2" color="text.secondary" align="center">
+        My best player
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center">
+        Duration: {duration.toFixed(2)} Seek: {seek.toFixed(2)} isLoaded: {isLoaded ? 'loaded' : 'loading'}
+      </Typography>
+      <Button onClick={() => handleClick()}>
+        {isPlaying ? <Pause/> : <PlayArrow/>}
+      </Button>
+      <ReactHowler
+        src={musicFile}
+        playing={isPlaying}
+        onPause={() => handlePause()}
+        onEnd={() => console.log('end')}
+        onLoad={() => handleOnLoad()}
+        ref={(ref) => (player = ref)}
+      />
+    </Container>)
+}
+export default Index
